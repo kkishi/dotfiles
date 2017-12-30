aa8c619f82881a75e465b6ff8c5ed9b8e18e17ff
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2017-12-30 14:46:29 +0900

    Revert -f option

--- link.sh
+++ link.sh
@@ -3,6 +3,6 @@
 for file in $(ls -a ~/dotfiles); do
   if test -f ~/dotfiles/$file && [[ $file == .* ]]; then
       echo "Linking $file"
-      ln -s -f ~/dotfiles/$file ~/$file;
+      ln -s ~/dotfiles/$file ~/$file;
   fi;
 done
