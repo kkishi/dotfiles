88c74d2a86be45e58a44d3b98c4a374fd062c822
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2017-12-30 07:40:33 +0000

    Add bazelrc

--- link.sh
+++ link.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 for file in $(ls -a ~/dotfiles); do
-  if test -f ~/dotfiles/$file && [[ $file == .* ]]; then
-      echo "Linking $file"
+  if [[ -f ~/dotfiles/$file ]] && [[ $file == .* ]]; then
+      if [[ -f ~/$file ]]; then
+        echo "$file already exists; skipping"
+        continue
+      fi
       ln -s ~/dotfiles/$file ~/$file;
+      echo "Linked $file"
   fi;
 done
