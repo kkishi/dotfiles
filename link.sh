fd8caba79cc362dbd56b8e4c363a9c2d445d5611
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-02-03 21:30:15 +0100

    keep old file

--- link.sh
+++ link.sh
@@ -2,9 +2,13 @@
 
 for file in $(ls -a ~/dotfiles); do
   if [[ -f ~/dotfiles/$file ]] && [[ $file == .* ]]; then
-      if [[ -f ~/$file ]]; then
-        echo "$file already exists; skipping"
+      if [[ -L ~/$file ]]; then
+        echo "$file already exists as a symlink; skip"
         continue
+      fi
+      if [[ -f ~/$file ]]; then
+        echo "$file already exists as a regular file; keeping it as ~/$file.old"
+        mv ~/$file ~/$file.old
       fi
       ln -s ~/dotfiles/$file ~/$file;
       echo "Linked $file"
